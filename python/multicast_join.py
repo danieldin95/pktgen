7f4b552992c886b73efebda95d1b90af7626e2ba
Daniel Ding <zhihui.ding@easystack.cn>
2024-12-05 16:16:20 +0800

    python: send igmpv3 with iface address.

--- python/multicast_join.py
+++ python/multicast_join.py
@@ -2,6 +2,7 @@
 
 import time
 import argparse
+import subprocess
 
 from scapy.all import sendp
 from scapy.all import Ether, IP
@@ -9,10 +10,25 @@ from scapy.contrib.igmp import IGMP
 from scapy.contrib.igmpv3 import IGMPv3, IGMPv3mr, IGMPv3gr
 
 
+def get_address(name):
+
+    output = subprocess.check_output(["ip", "addr", "show", name], encoding="utf-8")
+    mac = None
+    addr = None
+    for line in output.splitlines():
+        values = line.strip().split()
+        if not mac and values[0] == "link/ether":
+            mac = values[1]
+        elif not addr and values[0] == "inet":
+            addr = values[1].split('/')[0]
+    return mac, addr
+
+
 def run(group, iface="eth0", interval=15, ver="v3"):
     """"""
-    a = Ether()
-    b = IP()
+    mac, addr = get_address(iface)
+    a = Ether(src=mac)
+    b = IP(src=addr)
 
     if ver == "v1":
         c = IGMP(type=0x12, gaddr=group)
